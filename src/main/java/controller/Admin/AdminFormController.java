ae77f8e82151b9dc12da6ba2eaa3e1693f75f96a
bavindushan <bavindushan65@gmail.com>
2025-01-17 17:54:36 +0530

    adminform interface complete

---
+++ src/main/java/controller/Admin/AdminFormController.java
@@ -0,0 +1,58 @@
+package controller.Admin;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class AdminFormController {
+
+    @FXML
+    private TableColumn clmEmail;
+
+    @FXML
+    private TableColumn clmID;
+
+    @FXML
+    private TableColumn clmName;
+
+    @FXML
+    private TableColumn clmPassword;
+
+    @FXML
+    private TableView tblAdminTable;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
