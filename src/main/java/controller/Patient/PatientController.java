9dfab55d254994210448b498da9bc60659074bf7
bavindushan <bavindushan65@gmail.com>
2025-01-22 18:32:21 +0530

    addPatient method complete

--- src/main/java/controller/Patient/PatientController.java
+++ src/main/java/controller/Patient/PatientController.java
@@ -1,19 +1,47 @@
 package controller.Patient;
 
+import db.DBConnection;
 import model.Patient;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class PatientController implements PatientServices {
 
     @Override
-    public boolean addPatient(Patient patient) {
-        return false;
+    public boolean addPatient(Patient patient) throws SQLException {
+        String SQL = "INSERT INTO patient (patient_id,,name,age,gender,contact_number,medical_history) VALUES (?,?,?,?,?,?)";
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+        preparedStatement.setString(1, patient.getID());
+        preparedStatement.setString(2, patient.getName());
+        preparedStatement.setString(3, patient.getAge());
+        preparedStatement.setString(4, patient.getGender());
+        preparedStatement.setString(5, patient.getTelNo());
+        preparedStatement.setString(6, patient.getMedicalHistory());
+
+        int affectedrows = preparedStatement.executeUpdate();
+        return affectedrows >0;
     }
 
     @Override
-    public Patient searchPatient(String TelNo) {
-        return null;
+    public Patient searchPatient(String TelNo) throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM patient WHERE contact_number=" + "'" + TelNo + "'");
+        resultSet.next();
+
+        return new Patient(
+                resultSet.getString(1),
+                resultSet.getString(2),
+                resultSet.getString(3),
+                resultSet.getString(4),
+                resultSet.getString(5),
+                resultSet.getString(6)
+        );
     }
 
     @Override
--- src/main/java/controller/Patient/PatientServices.java
+++ src/main/java/controller/Patient/PatientServices.java
@@ -2,11 +2,12 @@ package controller.Patient;
 
 import model.Patient;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface PatientServices {
-    boolean addPatient(Patient patient);
-    Patient searchPatient(String TelNo);
+    boolean addPatient(Patient patient) throws SQLException;
+    Patient searchPatient(String TelNo) throws SQLException;
     boolean updatePatient(Patient patient);
     boolean deletePatient(String TelNo);
     List<Patient> getAll();
