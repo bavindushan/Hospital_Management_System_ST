1a224fc105e11f9be2257c0960bb52e303518b66
bavindushan <bavindushan65@gmail.com>
2025-01-27 12:21:17 +0530

    schedule management front-end complete

---
+++ src/main/java/controller/Schedule/ScheduleFormController.java
@@ -0,0 +1,64 @@
+package controller.Schedule;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ScheduleFormController {
+
+    @FXML
+    private TableColumn<?, ?> clmDoctorID;
+
+    @FXML
+    private TableColumn<?, ?> clmScheduleDetails;
+
+    @FXML
+    private TableColumn<?, ?> clmStaffID;
+
+    @FXML
+    private TableColumn<?, ?> clmscheduleID;
+
+    @FXML
+    private ComboBox<?> cmbDoctorID;
+
+    @FXML
+    private ComboBox<?> cmbSchedulesList;
+
+    @FXML
+    private ComboBox<?> cmbStaffID;
+
+    @FXML
+    private TableView<?> tblScheduleManagemenet;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
