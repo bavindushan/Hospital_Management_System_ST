45eef84f011f5b6cf7b4e41fc04100af89cfa246
bavindushan <bavindushan65@gmail.com>
2025-02-03 15:18:20 +0530

    btnAppointmentsOnAction method complete

---
+++ src/main/java/controller/Appointment/AppointmentFormController.java
@@ -0,0 +1,4 @@
+package controller.Appointment;
+
+public class AppointmentFormController {
+}
--- src/main/java/controller/dashboards/ReceptionistDashBoardFormController.java
+++ src/main/java/controller/dashboards/ReceptionistDashBoardFormController.java
@@ -2,7 +2,12 @@ package controller.dashboards;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class ReceptionistDashBoardFormController {
 
@@ -10,8 +15,13 @@ public class ReceptionistDashBoardFormController {
     private AnchorPane ancpReceptionistDashBoard;
 
     @FXML
-    void btnAppointmentsOnAction(ActionEvent event) {
+    void btnAppointmentsOnAction(ActionEvent event) throws IOException {
+        URL resource = this.getClass().getResource("view/AppointmenetsManager.fxml");
 
+        assert resource != null;
+        Parent load = FXMLLoader.load(resource);
+        ancpReceptionistDashBoard.getChildren().clear();
+        ancpReceptionistDashBoard.getChildren().add(load);
     }
 
     @FXML
