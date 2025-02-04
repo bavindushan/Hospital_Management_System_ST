b0ab64a0ceed2fff5ba2dc2083dc50356edbbf24
bavindushan <bavindushan65@gmail.com>
2025-02-04 13:04:29 +0530

    AppointmentServices interface complete

--- src/main/java/controller/Appointment/AppointmentController.java
+++ src/main/java/controller/Appointment/AppointmentController.java
@@ -1,4 +1,37 @@
 package controller.Appointment;
 
-public class AppointmentController {
+import model.Appointment;
+
+import java.util.List;
+
+public class AppointmentController implements AppointmentServices{
+    @Override
+    public boolean addAppointment(Appointment appointment) {
+        return false;
+    }
+
+    @Override
+    public boolean UpdateAppointment(Appointment appointment) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteAppointment(String ID) {
+        return false;
+    }
+
+    @Override
+    public Appointment searchAppointment(String ID) {
+        return null;
+    }
+
+    @Override
+    public List<Appointment> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public String getLastID() {
+        return "";
+    }
 }
--- src/main/java/controller/Appointment/AppointmentServices.java
+++ src/main/java/controller/Appointment/AppointmentServices.java
@@ -1,4 +1,14 @@
 package controller.Appointment;
 
+import model.Appointment;
+
+import java.util.List;
+
 public interface AppointmentServices {
+    boolean addAppointment(Appointment appointment);
+    boolean UpdateAppointment(Appointment appointment);
+    boolean deleteAppointment(String ID);
+    Appointment searchAppointment(String ID);
+    List<Appointment> getAll();
+    String getLastID();
 }
