7d6f21862722b22374baec1770074822f3945255
bavindushan <bavindushan65@gmail.com>
2025-02-10 16:04:57 +0530

    add method complete in billing and payment class

--- src/main/java/controller/BillingAndPayments/BillingAndPaymentController.java
+++ src/main/java/controller/BillingAndPayments/BillingAndPaymentController.java
@@ -1,13 +1,30 @@
 package controller.BillingAndPayments;
 
+import db.DBConnection;
 import model.PaymentBill;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class BillingAndPaymentController implements BillingAndPaymentServices{
     @Override
-    public boolean add(PaymentBill paymentBill) {
-        return false;
+    public boolean add(PaymentBill paymentBill) throws SQLException {
+        String SQL = "INSERT INTO billing (bill_id,patient_id,total_amount,payment_status,invoice_pdf,generated_date) VALUES (?,?,?,?,?,?)";
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+        preparedStatement.setString(1, paymentBill.getId());
+        preparedStatement.setString(2, paymentBill.getPatientID());
+        preparedStatement.setDouble(3,paymentBill.getTotal());
+        preparedStatement.setString(4, paymentBill.getStatus());
+        preparedStatement.setString(5, paymentBill.getInvoiceName());
+        preparedStatement.setDate(6, Date.valueOf(paymentBill.getDate()));
+
+        int affectedRows = preparedStatement.executeUpdate();
+        return affectedRows>0;
     }
 
     @Override
--- src/main/java/controller/BillingAndPayments/BillingAndPaymentServices.java
+++ src/main/java/controller/BillingAndPayments/BillingAndPaymentServices.java
@@ -2,10 +2,11 @@ package controller.BillingAndPayments;
 
 import model.PaymentBill;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface BillingAndPaymentServices {
-    boolean add(PaymentBill paymentBill);
+    boolean add(PaymentBill paymentBill) throws SQLException;
     boolean update(PaymentBill paymentBill);
     boolean delete(String id);
     PaymentBill search(String id);
