b21930ebffc1ce9b899ea96ffccbf1d9314388f8
bavindushan <bavindushan65@gmail.com>
2025-02-11 16:32:56 +0530

    loadTable method complete

--- src/main/java/controller/AppointmentAndAvailability/AppointmentAndAvailabilityFormController.java
+++ src/main/java/controller/AppointmentAndAvailability/AppointmentAndAvailabilityFormController.java
@@ -1,4 +1,81 @@
 package controller.AppointmentAndAvailability;
 
-public class AppointmentAndAvailabilityFormController {
+import controller.Appointment.AppointmentController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Appointment;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class AppointmentAndAvailabilityFormController implements Initializable {
+
+    @FXML
+    private TableColumn clmDate;
+
+    @FXML
+    private TableColumn clmDoctorID;
+
+    @FXML
+    private TableColumn clmID;
+
+    @FXML
+    private TableColumn clmPatientID;
+
+    @FXML
+    private TableColumn clmSatus;
+
+    @FXML
+    private TableColumn clmTime;
+
+    @FXML
+    private ComboBox cmbStatus;
+
+    @FXML
+    private TableView tblAppointments;
+
+    @FXML
+    private TextField txtId;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadTable();
+    }
+    private void loadTable(){
+
+        clmID.setCellValueFactory(new PropertyValueFactory<>("id"));
+        clmPatientID.setCellValueFactory(new PropertyValueFactory<>("patientID"));
+        clmDoctorID.setCellValueFactory(new PropertyValueFactory<>("doctorID"));
+        clmDate.setCellValueFactory(new PropertyValueFactory<>("date"));
+        clmTime.setCellValueFactory(new PropertyValueFactory<>("time"));
+        clmSatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+
+        AppointmentController appointmentController = new AppointmentController();
+        List<Appointment> all = appointmentController.getAll();
+        ObservableList<Appointment> observableList = FXCollections.observableArrayList();
+
+        all.forEach(appointment -> observableList.add(appointment));
+        tblAppointments.setItems(observableList);
+    }
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+
 }
