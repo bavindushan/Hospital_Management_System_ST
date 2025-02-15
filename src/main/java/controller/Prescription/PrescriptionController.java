c480decf37d03935f993ed412a97a26fece012bc
bavindushan <bavindushan65@gmail.com>
2025-02-15 21:57:16 +0530

    update(Prescription method complete

--- src/main/java/controller/Prescription/PrescriptionController.java
+++ src/main/java/controller/Prescription/PrescriptionController.java
@@ -34,8 +34,27 @@ public class PrescriptionController implements PrescriptionServices{
     }
 
     @Override
-    public boolean update(Prescription prescription) {
-        return false;
+    public boolean update(Prescription prescription) throws SQLException {
+        String SQL = "UPDATE prescription SET patient_id=?,doctor_id=?,dosage=?,duration=?,medicine_details=?,additional_notes=? WHERE prescription_id=?";
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+        preparedStatement.setString(1,prescription.getPatient_id());
+        preparedStatement.setString(2,prescription.getDoctor_id());
+        preparedStatement.setString(3,prescription.getDosage());
+        preparedStatement.setString(4,prescription.getDuration());
+        preparedStatement.setString(5,prescription.getMedicine_details());
+
+        //null handling in additional notes
+        if (prescription.getAdditional_notes() != null) {
+            preparedStatement.setString(6, prescription.getAdditional_notes());
+        } else {
+            preparedStatement.setNull(6, Types.VARCHAR); // Safe way to insert NULL
+        }
+
+        preparedStatement.setString(7,prescription.getId());
+
+        int affectedrows = preparedStatement.executeUpdate();
+        return affectedrows>0;
     }
 
     @Override
--- src/main/java/controller/Prescription/PrescriptionServices.java
+++ src/main/java/controller/Prescription/PrescriptionServices.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface PrescriptionServices {
     boolean add(Prescription prescription) throws SQLException;
-    boolean update(Prescription prescription);
+    boolean update(Prescription prescription) throws SQLException;
     boolean delete(String id);
     Prescription search(String id);
     List<Prescription> getAll();
